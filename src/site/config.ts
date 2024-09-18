aa8766b548cbf30f48702e0910d6f94b27c025b1
tocaku <bogiveler@gmail.com>
2024-09-18 22:38:38 +0300

    first commit

---
+++ src/site/config.ts
@@ -0,0 +1,4 @@
+export const githubUrl = "https://github.com/tocaku";
+export const linkedinUrl =
+  "https://www.linkedin.com/in/tocaku-btv-804797281/?originalSubdomain=ua";
+export const productionUrl = `https://github.com/tocaku`;
